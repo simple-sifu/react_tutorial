80257e1bd5f6ac9ac50e272e7ef1f65370056ea0
Tommy Han <tommy_han1@intuit.com>
2021-05-22 23:22:28 -0400

    initial version with webpack starter

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.join(__dirname, '/build'),
+        filename: 'index.bundle.js',
+    },
+    devServer: {
+        port: 3010,
+        watchContentBase: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                resolve: {
+                    extensions: ['.js', '.jsx'],
+                },
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.(scss|css)$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader'
+                ],
+            }
+        ]
+    },
+    plugins: [new MiniCssExtractPlugin()],
+}
