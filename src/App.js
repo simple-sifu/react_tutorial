278b336fde15b2b0017bc011445b3b25e7a62c9e
Tommy Han <tommy_han1@intuit.com>
2021-05-22 23:44:32 -0400

    button changes state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export function App() {
-    return (
-        <div>
-            <h1>Welcome { new Date().toString() } </h1>
-        </div>
-    );
+export class App extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            name: "Tommy"
+        }
+
+    }
+
+    render(){
+        return (
+            <div>
+                <h1>Welcome { this.state.name } </h1>
+                <button onClick={ () => this.setState({name: "Jeremy"})} > change name</button>
+            </div>
+        );
+    }
+
 }
